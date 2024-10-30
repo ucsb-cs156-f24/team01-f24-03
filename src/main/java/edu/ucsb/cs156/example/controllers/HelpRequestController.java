144c81b78f6690f5ce32dc68aa9664b16029c1c5
Bharat Saiju <bsaiju@ucsb.edu>
2024-10-29 19:37:02 -0700

    changing table name to helprequests

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
@@ -34,7 +34,7 @@ import java.time.LocalDateTime;
  * This is a REST controller for HelpRequests
  */
 
-@Tag(name = "HelpRequests")
+@Tag(name = "helprequests")
 @RequestMapping("/api/helprequests")
 @RestController
 @Slf4j
